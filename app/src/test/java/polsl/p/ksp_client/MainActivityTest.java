fa73fca068196224b54c767e44d1db9b81b2b0ce
Bartosz Gawron <bartho58@gmail.com>
2019-07-04 20:52:48 +0200

    Final

--- app/src/test/java/polsl/p/ksp_client/MainActivityTest.java
+++ app/src/test/java/polsl/p/ksp_client/MainActivityTest.java
@@ -1,7 +1,7 @@
 package polsl.p.ksp_client;
 
 import com.android.volley.toolbox.JsonArrayRequest;
-
+import static org.junit.Assert.assertEquals;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -29,8 +29,19 @@ public class MainActivityTest {
         return null;
     }
 
+    @Test
+    public  void contructorMeasurement(){
+       Measurement measurement = new Measurement(1.1,2.2,3.3);
+
+        assertEquals("1.1", measurement.getTemperature().toString());
+        assertEquals("2.2", measurement.getHumidity().toString());
+        assertEquals("3.3", measurement.getPressure().toString());
+
+    }
+
     String urlForGetRequest = "http://10.0.2.2:8080/test";
     String actual = "{\"temperature\":1.1,\"humidity\":1.2,\"pressure\":3.2}";
+
 
     @Test
     public void onCreate() {
